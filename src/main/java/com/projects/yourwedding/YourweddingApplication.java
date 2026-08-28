f27d4e9d03ecbe1d8530d40f75a231955ab18611
pedrosardanha <ph264915@gmail.com>
2026-08-28 20:22:39 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/projects/yourwedding/YourweddingApplication.java
@@ -0,0 +1,13 @@
+package com.projects.yourwedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourweddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourweddingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/yourwedding/YourweddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.yourwedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourweddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
